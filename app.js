786fb3959615d0d9fd8cbebf916a0eb856b90499
rajkiran7117 <raj.kiran182007@gmail.com>
2019-08-30 11:42:14 +0530

    commit hello v2

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 var express = require('express')
 var app = express()
 const port = process.env.PORT || 5000;
-//var EnvName = process.env.EnvName;
+var EnvName = process.env.EnvName;
 
 app.get('/', (req, res) =>{
   var htmlString ='Hello World';
-  
+  if(EnvName){
+   htmlString += ' '+EnvName; 
+  }
   
   
   res.send(htmlString);

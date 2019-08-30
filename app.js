d0d38f6277982d0f77fc234317dedf0ed99b6e55
rajkiran7117 <raj.kiran182007@gmail.com>
2019-08-30 11:25:40 +0530

    commit v2

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var express = require('express')
 var app = express()
 const port = process.env.PORT || 5000;
-var EnvName = process.env.EnvName;
+//var EnvName = process.env.EnvName;
 
-app.get('/', async (req, res) =>{
+app.get('/', (req, res) =>{
   var htmlString ='<html><title>Heroku Demo</title><body><h1>Hello World </h1></body></html>';
   
   
